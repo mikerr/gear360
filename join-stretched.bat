fa19f57c62691d7c543d80d076fa8b350f9f34da
Mike Redrobe <mikeredrobe@gmail.com>
2026-04-25 11:32:55 +0000

    Enhance join-stretched.bat to merge metadata
    Added exiftool command to merge metadata from pano.xml into output.mp4 and delete both mylist.txt and output.mp4 after processing.

--- join-stretched.bat
+++ join-stretched.bat
@@ -2,5 +2,6 @@ for %%i in (*stretched.mp4) do (
 	@echo file '%%i' >> mylist.txt
 )
 ffmpeg -f concat -i mylist.txt -c copy output.mp4
-del mylist.txt
+exiftool -tagsfromfile pano.xml -api largefilesupport=1 -all:all -o output.mp4 merged.mp4
+del mylist.txt output.mp4
 pause
