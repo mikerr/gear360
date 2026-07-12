8524fa7ebcf92f0ee050b2c88d3165d326dea029
mikerr <mike@redrobe.net>
2026-07-12 20:27:33 +0100

    linux support

--- stretch-cpu.sh
+++ stretch-cpu.sh
@@ -2,5 +2,5 @@
 for FILE in *.MP4 ; do
     [ -f "$FILE" ] || break
     ffmpeg -y -i $FILE -vf v360=dfisheye:e:yaw=0:ih_fov=192:iv_fov=192 -c:v libx264 -b:v 40000k -bufsize 5000k -c:a copy out.mp4
-    exiftool -tagsfromfile pano.xml -api largefilesupport=1 -all:all -o %%~nf-stretched.mp4 out.mp4
+    exiftool -tagsfromfile pano.xml -api largefilesupport=1 -all:all -o $FILE-stretched.mp4 out.mp4
 done
